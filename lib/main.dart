af7b3fb783e304b9e71b4c5f2438bceba29564e3
Stwn25 <msetiawanwibisono@gmail.com>
2025-07-29 01:48:47 +0700

    fixed

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:provider/provider.dart';
 import 'package:testing/pages/authPage/loginPage.dart';
 import 'package:testing/pages/authPage/registrasiPage.dart';
 import 'package:testing/pages/authPage/registrasiPageDua.dart';
 import 'package:testing/pages/authPage/registrasiPageSatu.dart';
+import 'package:testing/pages/chatbotPage/chatbotPage.dart';
 import 'package:testing/pages/navbar.dart';
 import 'package:testing/pages/starterPage/splashPage.dart';
 import 'package:testing/pages/starterPage/starterPageSatu.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:testing/providers/chatbotProvider.dart';
 
 void main() {
   SystemChrome.setSystemUIOverlayStyle(
@@ -16,7 +19,12 @@ void main() {
       statusBarIconBrightness: Brightness.dark,
     ),
   );
-  runApp(const MyApp());
+  runApp(MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => ChatbotProvider()),
+      ],
+      child: MyApp(),
+    ),);
 }
 
 class MyApp extends StatelessWidget {
@@ -29,20 +37,18 @@ class MyApp extends StatelessWidget {
       minTextAdapt: true,
       splitScreenMode: true,
       builder: (_, child) {
-        return WillPopScope(
-          onWillPop: () async => false,
-          child: MaterialApp(
-            debugShowCheckedModeBanner: false,
-            home: SplashPage(),
-            routes: {
-              StarterPageSatu.routeName: (context) => StarterPageSatu(),
-              LoginPage.routeName: (context) => LoginPage(),
-              RegistrasiPage.routeName: (context) => RegistrasiPage(),
-              RegistrasiPageSatu.routeName: (context) => RegistrasiPageSatu(),
-              RegistrasiPageDua.routeName: (context) => RegistrasiPageDua(),
-              Navbar.routeName: (context) => Navbar(),
-            },
-          ),
+        return MaterialApp(
+          debugShowCheckedModeBanner: false,
+          home: SplashPage(),
+          routes: {
+            StarterPageSatu.routeName: (context) => StarterPageSatu(),
+            LoginPage.routeName: (context) => LoginPage(),
+            RegistrasiPage.routeName: (context) => RegistrasiPage(),
+            RegistrasiPageSatu.routeName: (context) => RegistrasiPageSatu(),
+            RegistrasiPageDua.routeName: (context) => RegistrasiPageDua(),
+            Navbar.routeName: (context) => Navbar(),
+            ChatbotPage.routeName: (context) => ChatbotPage(),
+          },
         );
       },
     );
